f8711516932c3494e28809c10166a99bda4d3cbf
Diego220581 <Diego.gomes.legal.br.cia@gmail.com>
2026-05-15 09:06:34 -0300

    nasser.sh

--- nasser.sh
+++ nasser.sh
@@ -1,22 +1,26 @@
 #!/data/data/com.termux/files/usr/bin/bash
 clear
 echo -e "\033[1;36m[!] ACESSANDO SERVIDOR NASSER SS V3...\033[0m"
-
-# Instalação rápida de dependências se não houver
 pkg install android-tools ncurses-utils -y > /dev/null 2>&1
 
-# Baixa o binário e a biblioteca necessária do seu repositório
-curl -L -o nasser_v3_bin https://github.com/Diego220581/NasserSS/raw/main/nasser_v3_bin > /dev/null 2>&1
-curl -L -o libc++_shared.so https://github.com/Diego220581/NasserSS/raw/main/libc++_shared.so > /dev/null 2>&1 2>/dev/null || cp /data/data/com.termux/files/usr/lib/libc++_shared.so .
+# Interface de Conexão Inclusa
+echo -e "\n\033[1;33m[1/2] PAREAMENTO WI-FI\033[0m"
+printf "\033[1;37mIP:Porta Pareamento: \033[0m"; read PAIR
+printf "\033[1;37mCódigo 6 Dígitos: \033[0m"; read CODE
+adb pair $PAIR <<< "$CODE"
 
-# Injeção e Execução via ADB
-echo -e "\033[1;33m[!] Estabelecendo ponte ADB nativa...\033[0m"
-adb push libc++_shared.so /data/local/tmp/ > /dev/null 2>&1
+echo -e "\n\033[1;33m[2/2] CONEXÃO PRINCIPAL\033[0m"
+printf "\033[1;37mIP:Porta Principal: \033[0m"; read CONNECT
+adb connect $CONNECT
+
+# Baixa e Injeta
+echo -e "\n\033[1;32m[!] Injetando Core Engine no Kernel...\033[0m"
+curl -L -o nasser_v3_bin https://github.com/Diego220581/NasserSS/raw/main/nasser_v3_bin > /dev/null 2>&1
 adb push nasser_v3_bin /data/local/tmp/ > /dev/null 2>&1
 adb shell chmod +x /data/local/tmp/nasser_v3_bin
 
-# Execução do Motor C++
-adb shell "LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/nasser_v3_bin"
+# Executa
+adb shell /data/local/tmp/nasser_v3_bin
 
-# Limpeza automática
-rm nasser_v3_bin libc++_shared.so nasser.sh > /dev/null 2>&1
+# Limpa tudo
+rm nasser_v3_bin
